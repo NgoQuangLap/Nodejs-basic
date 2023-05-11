dafef76079b04e177a0b0635e77ba33f89e20e7f
ngoquanglap0612 <ngoquanglap0612@gmail.com>
2023-05-11 17:08:41 +0700

    refactor code: create routes and controller

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+// Use route
+const route = require('./routes')
+
 // Setup file static, ex: img, ...
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
+
+// Use middleware
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
 
 //Temple engine
 app.engine('hbs', handlebars.engine({
@@ -20,14 +29,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Router init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+
+const route = (app) => {
+    // call router news
+    app.use('/news', newsRouter);
+
+    // call router home
+    app.use('/', siteRouter);
+
+    // call router search
+    app.use('/search', siteRouter);
+}
+
+module.exports = route;
